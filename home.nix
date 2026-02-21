4ca55c4268220759c8a562d8d22befab41ad08ad
kenh0u <21225079+kenh0u@users.noreply.github.com>
2026-02-21 15:36:54 +0900

    add PS1

--- home.nix
+++ home.nix
@@ -19,6 +19,12 @@
   programs.bash = {
     enable = true;
     initExtra = ''
+      if [ "$TERM" != "dumb" ]; then
+        PROMPT_COLOR="1;31m"
+        ((UID)) && PROMPT_COLOR="1;32m"
+        PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+      fi
+
       if [ -f ~/.bash_aliases ]; then
         . ~/.bash_aliases
       fi
