1c9913ff7a28a25caf245d8d6cabe59b91f2b742
kenh0u <21225079+kenh0u@users.noreply.github.com>
2026-03-02 21:10:42 +0900

    nix develop prompt

--- home.nix
+++ home.nix
@@ -20,9 +20,27 @@
     enable = true;
     initExtra = ''
       if [ "$TERM" != "dumb" ]; then
-        PROMPT_COLOR="1;31m"
-        ((UID)) && PROMPT_COLOR="1;32m"
-        PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+        _update_ps1() {
+          local PROMPT_COLOR="1;31m"
+          ((UID)) && PROMPT_COLOR="1;32m"
+
+          local NIX_PROMPT=""
+          if [ -n "$IN_NIX_SHELL" ]; then
+            local ENV_NAME="nix"
+            if [ -n "$pname" ]; then
+              ENV_NAME="$pname"
+            elif [ -n "$name" ]; then
+              ENV_NAME="''${name%-env}"
+            fi
+            NIX_PROMPT="\[\033[1;36m\]($ENV_NAME)\[\033[0m\] "
+          fi
+
+          PS1="\n$NIX_PROMPT\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+        }
+
+        if [[ "$PROMPT_COMMAND" != *"_update_ps1"* ]]; then
+          PROMPT_COMMAND="_update_ps1; ''${PROMPT_COMMAND:-}"
+        fi
       fi
 
       if [ -f ~/.bash_aliases ]; then
